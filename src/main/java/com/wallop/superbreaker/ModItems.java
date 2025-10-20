8746500fd32f8e80bb1d9f9e656494f3d01d1768
Wallop T. (Pom) <wallop.t@pomelofashion.com>
2025-10-20 23:08:32 +0700

    chore(item): Update new item

--- src/main/java/com/wallop/superbreaker/ModItems.java
+++ src/main/java/com/wallop/superbreaker/ModItems.java
@@ -2,19 +2,17 @@ package com.wallop.superbreaker;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.ToolItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item SUPERBREAKER = new ToolItem(
-        new SuperMaterial(),
-        new Item.Settings().maxDamage(500).arch$group(ItemGroups.TOOLS)
+    public static final Item SUPERBREAKER = new Item(
+        new Item.Settings().maxDamage(500)
     );
 
     public static void registerItems() {
-        Registry.register(Registries.ITEM, new Identifier("superbreaker", "superbreaker"), SUPERBREAKER);
+        Registry.register(Registries.ITEM, Identifier.of("superbreaker", "superbreaker"), SUPERBREAKER);
     }
 }
